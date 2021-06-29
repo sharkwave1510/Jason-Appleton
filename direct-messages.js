fe229459b1561b0a22c738b8830970546ed46037
sharkwave1510 <53194033+sharkwave1510@users.noreply.github.com>
2021-06-29 15:40:56 -0700

    Add files via upload

---
+++ direct-messages.js
@@ -0,0 +1,68 @@
+window.YTD.direct_messages.part0 = [ {
+  "dmConversation" : {
+    "conversationId" : "352130658-864627542",
+    "messages" : [ {
+      "messageCreate" : {
+        "recipientId" : "864627542",
+        "reactions" : [ ],
+        "urls" : [ ],
+        "text" : "Hello 💓",
+        "mediaUrls" : [ ],
+        "senderId" : "352130658",
+        "id" : "1272621161336184845",
+        "createdAt" : "2020-06-15T20:05:03.748Z"
+      }
+    } ]
+  }
+}, {
+  "dmConversation" : {
+    "conversationId" : "864627542-936464867728470016",
+    "messages" : [ {
+      "messageCreate" : {
+        "recipientId" : "864627542",
+        "reactions" : [ ],
+        "urls" : [ ],
+        "text" : "Let me inform you that person you are following @Princess26Tweet is a FAKE account. It is a fucking bastard from India who pretends to be a woman!\n\nWhen YOU follow @Princess26Tweet FAKE account, like and retweet, YOU promote his page, YOU become accomplice &amp; YOU help him trick others. Please, think about it.",
+        "mediaUrls" : [ ],
+        "senderId" : "936464867728470016",
+        "id" : "1290937875991863302",
+        "createdAt" : "2020-08-05T09:09:08.846Z"
+      }
+    }, {
+      "messageCreate" : {
+        "recipientId" : "864627542",
+        "reactions" : [ ],
+        "urls" : [ ],
+        "text" : "Hello 👋",
+        "mediaUrls" : [ ],
+        "senderId" : "936464867728470016",
+        "id" : "1290937240810643460",
+        "createdAt" : "2020-08-05T09:06:37.384Z"
+      }
+    } ]
+  }
+}, {
+  "dmConversation" : {
+    "conversationId" : "864627542-1250917610960666631",
+    "messages" : [ {
+      "messageCreate" : {
+        "recipientId" : "864627542",
+        "reactions" : [ ],
+        "urls" : [ {
+          "url" : "https://t.co/OoOHqD8MFF",
+          "expanded" : "http://suckingteens.club/ShBUOVMV59Wxps1",
+          "display" : "suckingteens.club/ShBUOVMV59Wxps1"
+        }, {
+          "url" : "https://t.co/mIRTcUqist",
+          "expanded" : "https://twitter.com/messages/media/1279329838562783236",
+          "display" : "pic.twitter.com/mIRTcUqist"
+        } ],
+        "text" : "Hey there sexy , I hope you like my naughty photo 👉👌 I've broke up with my boyfriend two weeks ago and I am so bored today ... I really want to have some fun with you , but I can't do it on twitter since it's not working well for me so I made new profile at this site just for you, my id there is \"TricksyPoo_8229\" , signup here (its free don't worry😘😘) my acc is hidden so you have to verify your email and let me know here when you are done😙 - https://t.co/OoOHqD8MFF https://t.co/mIRTcUqist",
+        "mediaUrls" : [ "https://ton.twitter.com/dm/1279329838562783236/1279329781234966534/7T_yKTSu.jpg" ],
+        "senderId" : "1250917610960666631",
+        "id" : "1279329838562783236",
+        "createdAt" : "2020-07-04T08:22:57.166Z"
+      }
+    } ]
+  }
+} ]
